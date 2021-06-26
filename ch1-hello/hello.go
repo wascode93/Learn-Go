0bcad0e359ad13dcdeea438c57016330000ad6df
wascode93 <waseem93.kites@gmail.com>
2021-06-26 18:53:58 +0200

    - update hello function to display Hello, world if name param is not supplied

--- ch1-hello/hello.go
+++ ch1-hello/hello.go
@@ -9,5 +9,8 @@ func main() {
 }
 
 func hello(name string) string {
+	if name == "" {
+		name = "world"
+	}
 	return englishHelloPrefix + name
 }
--- ch1-hello/hello_test.go
+++ ch1-hello/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	expected := "Hello, Waseem"
-	actual := hello("Waseem")
-
-	if expected != actual {
-		t.Errorf("expected %q but was %q", expected, actual)
+	assertCorrectMessage := func(t testing.TB, expected, actual string) {
+		t.Helper()
+		if expected != actual {
+			t.Errorf("expected %q but was %q", expected, actual)
+		}
 	}
+
+	t.Run("Saying hello to people", func(t *testing.T) {
+		expected := "Hello, Waseem"
+		actual := hello("Waseem")
+		assertCorrectMessage(t, expected, actual)
+	})
+
+	t.Run("Say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		expected := "Hello, world"
+		actual := hello("")
+		assertCorrectMessage(t, expected, actual)
+	})
 }
